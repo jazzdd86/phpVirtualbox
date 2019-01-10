89e49ddfa17831a9005a6580deef82c382a0a7b8
Christian Gatzlaff <cgatzlaff@gmail.com>
2019-01-10 10:51:49 +0100

    corrected issue with multiple conf parameters

--- servers-from-env.php
+++ servers-from-env.php
@@ -1,20 +1,25 @@
 <?php
 
+// variables
 $servers = array();
 $config_overrides = array();
 
-// getting config overrides for all servers
+
+// getting global config overrides
 foreach ($_SERVER as $key => $value) {
     preg_match('/(.*?)(?:_ENV_|_)?CONF_(.+)/', $key, $matches);
     if ($matches) {
         $value= (strpos($value, ',')) ? split(',',$value) : $value;
-        $config_overrides[$matches[1]] = array($matches[2] => $value);
+        array_key_exists($matches[1], $config_overrides)?:$config_overrides[$matches[1]] = array();
+        $config_overrides[$matches[1]] += array($matches[2] => $value);
     }
 }
 
-echo 'Exposing the following linked server instances:' . PHP_EOL;
+echo 'Using following global configuration parameters:' . PHP_EOL;
+print_r($config_overrides[""]);
 
-// getting servers from linked vboxwebsrv containers or environment variables
+
+// getting servers from environment variables
 foreach ($_SERVER as $key => $value) {
     if (substr($key, -9) === '_HOSTPORT') {
         $prefix = substr($key, 0, -9);
@@ -32,13 +37,11 @@ foreach ($_SERVER as $key => $value) {
 
         $location = 'http://' . str_replace('tcp://', '', $value) . '/';
 
-        echo '- ' . $name . ' (' . $location .')' . PHP_EOL;
-
         $username = getenv($prefix.'_USER');
         $password = getenv($prefix.'_PW');
 
         if ($username == "") $username = 'username';
-        if ($password == "") $password = 'username';
+        if ($password == "") $password = 'password';
 
         $servers []= array_merge(array(
             'name' => $name,
@@ -48,16 +51,22 @@ foreach ($_SERVER as $key => $value) {
             (array_key_exists($prefix, $config_overrides)) ? $config_overrides[$prefix] : array());
     }
 }
+
+echo PHP_EOL.PHP_EOL.'Using the following linked server instances:'.PHP_EOL;
+print_r($servers);
+
+
 // check if there are any servers
 if (!$servers) {
-    echo 'Use environment variables to configure the correct connection!';
+    echo 'Use environment variables to configure the correct vboxwebsrv connection!';
     exit(1);
 }
 
-// put servers array to file
+
+// put servers array to file, which is then used in config.php of phpvirtualbox
 file_put_contents('/var/www/config-servers.php', '<?php return ' . var_export($servers, true) . ';');
 
+// put global config overrides into another file, which is then used in config.php of phpvirtualbox
 if (array_key_exists("", $config_overrides)) {
-    // getting global config overrides
     file_put_contents('/var/www/config-override.php','<?php return ' . var_export($config_overrides[""], true) . ';' );
 }
